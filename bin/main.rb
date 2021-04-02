6c95890abd90e4e52df8e2165506b185cc95cdcc
marwen-brini <brini.marwen@gmail.com>
2021-04-02 10:43:40 +0100

    first attempt at the game interface

--- bin/main.rb
+++ bin/main.rb
@@ -6,6 +6,8 @@ class Game
     @player2 = ''
     @players = {}
     @choices = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @player1_choices = []
+    @player2_choices = []
   end
 
   def draw_board
@@ -28,25 +30,46 @@ class Game
     puts "#{@player1} will use X and #{@player2} will use O "
   end
 
-  def run_turns
-    system 'clear'
-    puts 'its player 1 turn'
+  def user1_turn
+    puts "its #{@player1}'s turn"
     v = gets.chomp
+    while @player1_choices.include? v.to_i or @player2_choices.include? v.to_i
+      puts 'please another choice, you already used this one'
+      v = gets.chomp
+    end
+
     until v.to_i.between?(1, 9)
       puts 'please pick a choice between 1 and 9'
       v = gets.chomp
     end
     @choices[v.to_i - 1] = 'O'
+    @player1_choices.push(v.to_i)
     draw_board
+  end
 
-    puts 'its player 2 turn'
+  def user2_turn
+    puts "its #{@player2}'s turn"
     v = gets.chomp
     until v.to_i.between?(1, 9)
       puts 'please pick a choice between 1 and 9'
       v = gets.chomp
     end
     @choices[v.to_i - 1] = 'X'
+    @player2_choices.push(v.to_i)
+
     draw_board
+  end
+
+  def run_turns
+    system('clear')
+    i = 0
+    while i < 10
+
+      user1_turn
+      user2_turn
+
+    end
+    puts "it's a draw"
   end
 
   def run_game
